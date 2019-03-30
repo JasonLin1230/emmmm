cdee8e8e2600f9c6791ead52731a29e2d1790663
Jason Lin <JasonLin1230@163.com>
2019-03-30 08:36:37 +0800

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.get("/",(req,res) =>{
+    res.send("Hello World!");
+})
+
+const port = process.env.port || 5000;
+app.listen(port,() => {
+    console.log("Server running on port ${port}");
+})
