6d6f8ba66f17d43b96ed69d234a885cb94ccc095
Jason Lin <JasonLin1230@163.com>
2019-04-15 07:58:32 +0800

    leftMenu

--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -3,17 +3,22 @@
         <header-nav>
 
         </header-nav>
-        <router-view>
+        <left-menu>
+
+        </left-menu>
+        <router-view class="index-container">
 
         </router-view>
     </div>
 </template>
 <script>
 import myHeader from "../components/Header"
+import leftMenu from "../components/LeftMenu"
 export default {
     name:"index",
     components:{
-        "header-nav":myHeader
+        "header-nav":myHeader,
+        "left-menu":leftMenu
     }
 }
 </script>
@@ -22,6 +27,14 @@ export default {
     width: 100%;
     height: 100%;
     overflow: hidden;
+}
+.index-container{
+    position: absolute;
+    left: 180px;
+    top: 59px;
+    width: calc(100% - 180px);
+    height: calc(100% - 71px);
+    overflow: auto;
 }
 </style>
 
